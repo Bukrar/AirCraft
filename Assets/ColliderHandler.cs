960b1c40a77bc313ab4c31a515f1b685245bba84
Bukrar <alex968624229@gmail.com>
2019-05-07 00:14:17 +0800

    Enable GameOject

--- Assets/ColliderHandler.cs
+++ Assets/ColliderHandler.cs
@@ -2,16 +2,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ColliderHandler : MonoBehaviour
 {
+
+    [SerializeField] float levelLoadDelay = 1f;
+    [SerializeField] GameObject deathFX;
+
     private void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
+        deathFX.SetActive(true);
+        Invoke("ReLoad", levelLoadDelay);
     }
 
     private void StartDeathSequence()
     {
         SendMessage("OnPlayerDeath");
+    }
+
+    private void ReLoad()
+    {
+        SceneManager.LoadScene(1);
     }
 }
