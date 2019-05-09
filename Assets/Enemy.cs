01f6b894760831cdcc0d9efee34bea571998f4fc
Bukrar <alex968624229@gmail.com>
2019-05-09 23:32:26 +0800

    ADD CONPOMENT WITH CODE

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -7,6 +7,7 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
         
     }
 
