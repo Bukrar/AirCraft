eaa01aa3f8142635c8dcb1eb317ca88951b006c0
Bukrar <alex968624229@gmail.com>
2019-05-12 01:12:40 +0800

    Enemy Explosion Instaniate Explosion Destory Instaniate

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject deadFx;
+    [SerializeField] Transform save;
     void Start()
     {
         Collider boxCollider = gameObject.AddComponent<BoxCollider>();
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnParticleCollision(GameObject other)
     {
+        GameObject fx = Instantiate(deadFx, transform.position, Quaternion.identity);
+        fx.transform.parent = save;
         Destroy(gameObject);
     }
 }
